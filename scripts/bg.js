1b33f998117c0f406ef9115d0c0f2620429a5dfc
levelicarus <levelicarus@gmail.com>
2017-07-23 12:31:49 -0400

    Implemented parallax design, fixed home logo and nav bar menu, added randomization to variation and cell size for landing page

--- scripts/bg.js
+++ scripts/bg.js
@@ -1,9 +1,35 @@
-// Create the Trianglify image
+function rand_int(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //[min, max)
+}
+
+
+function rand_float(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+
+function rand_bg(index) {
+  bg = ['nhqip', '3fp01', 'yhojm', 'random']
+  return bg[index]
+}
+
+
+// Can customize color pattern:
+// https://www.npmjs.com/package/trianglify
 var pattern = Trianglify({
   height: window.innerHeight,
   width: window.innerWidth,
-  cell_size: 80});
+  // seed: rand_bg(rand_int(0,4)),
+  x_colors: 'random',
+  y_colors: 'match_x',
+  variance: rand_float(.7, 1),
+  stroke_width: rand_float(1, 5),
+  cell_size: rand_int(40, 200)});
 
-document.body.style.backgroundSize = "cover";
-document.body.style.backgroundImage =  "url(" + pattern.png() + ")";
-document.body.style.backgroundAttachment = "fixed";
+
+document.body.querySelector("#home").style.backgroundSize = "cover";
+document.body.querySelector("#home").style.backgroundImage =  "url(" + pattern.png() + ")";
+document.body.querySelector("#home").style.backgroundAttachment = "fixed";
+// document.body.querySelector("#home").style.opacity = 0.9;

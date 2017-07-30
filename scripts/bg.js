b5ca897ee452d53f91086563257875dd918fe1dc
levelicarus <levelicarus@gmail.com>
2017-07-30 10:25:31 -0400

    Made the navbar more minimalist by removing the transparent background. Added social/contacts to the top right-hand corner. Added a blog section. Put all members information into a JSON which can be used to read from later. Adjusted alignmnet for content. Aligned the footer info. Also landing page now only generates colors that allow the words under the logo to be readable.

--- scripts/bg.js
+++ scripts/bg.js
@@ -11,23 +11,52 @@ function rand_float(min, max) {
 
 
 function rand_bg(index) {
-  bg = ['nhqip', '3fp01', 'yhojm', 'random']
+  bg = ['nhqip', '3fp01', '3fp00', 'yhojm', 'yhojp', 'yhojt', 
+        'yhojw', 'yhojx', 'yhojy', 'yhoj3', 'yhoj4', 'yhoj6', 
+        'yhoj7', 'yhoj9']
+  // bg = halloweeen //if date ==
+  // bg = holidays   // if date == 
+  // bg = valentines // if date ==
   return bg[index]
 }
 
+// function get_date() {
+//   var today = new Date();
+//   var dd = today.getDate();
+//   var mm = today.getMonth();
+//   var yyyy = today.getFullYear();
+
+//   return
+// }
 
 // Can customize color pattern:
 // https://www.npmjs.com/package/trianglify
 var pattern = Trianglify({
   height: window.innerHeight,
   width: window.innerWidth,
-  // seed: rand_bg(rand_int(0,4)),
+  seed: rand_bg(rand_int(0,14)),
   x_colors: 'random',
   y_colors: 'match_x',
   variance: rand_float(.7, 1),
   stroke_width: rand_float(1, 5),
   cell_size: rand_int(40, 200)});
 
+// var today = new Date();
+
+// if today.getMonth() == 6 {
+//   pattern = Trianglify({
+//   color_function: function(x, y) {
+//     return  'hsl('+Math.floor(Math.abs(x*y)*360)+',80%,60%)';
+//   };
+//   height: window.innerHeight,
+//   width: window.innerWidth,
+//   seed: rand_bg(rand_int(0,14)),
+//   x_colors: 'random',
+//   y_colors: 'match_x',
+//   variance: rand_float(.7, 1),
+//   stroke_width: rand_float(1, 5),
+//   cell_size: rand_int(40, 200)});
+// }
 
 document.body.querySelector("#home").style.backgroundSize = "cover";
 document.body.querySelector("#home").style.backgroundImage =  "url(" + pattern.png() + ")";

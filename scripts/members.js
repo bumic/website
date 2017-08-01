e75e15a4a752256d6756a43cacf043050b6f4500
Zui <zuikng@gmail.com>
2017-08-01 00:03:54 -0400

    I need sleep

--- scripts/members.js
+++ scripts/members.js
@@ -1,26 +1,34 @@
-var members = [{
-    pic: "https://unsplash.it/500/500", name: "Justin Bieber"
-}, {
-    pic: "https://unsplash.it/500/500", name: "Justin Bieber"
-}, {
-    pic: "https://unsplash.it/500/500", name: "Justin Bieber"
-}, {
-    pic: "https://unsplash.it/500/500", name: "Justin Bieber"
-}, {
-    pic: "https://unsplash.it/500/500", name: "Justin Bieber"
-}, {
-    pic: "https://unsplash.it/500/500", name: "Justin Bieber"
-}, {
-    pic: "https://unsplash.it/500/500", name: "Justin Bieber"
-}, {
-    pic: "https://unsplash.it/500/500", name: "Justin Bieber"
-}];
+var members = [
+    {
+        pic: "https://unsplash.it/300/300", name: "Justin Bieber"
+    }
+];
 
 var vm = new Vue({
     el: '#members',
     data: {ms: members},
-    created: function () {
-        console.log(this.ms);
+    mounted: function () {
+        var m = fetch("/data/members.json")
+            .then(
+                resp => resp.json()
+            ).then(
+                json => {
+                    this.ms = json.members.map(function (t, a) {
+                        if (t.pic) {
+                            var p = `image/members/${t.pic}`;
+                            fetch(p).catch((e) => {
+                                p = "https://unsplash.it/500/500"
+                            });
+                        } else {
+                            var p = "https://unsplash.it/500/500";
+                        }
+                        return ({
+                            "name": t.first_name + " " + t.last_name,
+                            "pic": p
+                        });
+                    });
+                })
+        ;
     }
 });
 

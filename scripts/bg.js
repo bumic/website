4aacedac98b0366ccaf93d80abb2dc76ccc71fb3
Duy Nguyen <zuikng@gmail.com>
2017-07-31 20:53:08 -0400

    First step of mobile design
    * We change some of the structure of the CSS.
    * Made make pattern into a function
    
    Todo:
    * Begin working on the member section
    * Fill all of the parallax sections
    * Clean up the JavaScript

--- scripts/bg.js
+++ scripts/bg.js
@@ -1,23 +1,23 @@
 function rand_int(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min; //[min, max)
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min; //[min, max)
 }
 
 
 function rand_float(min, max) {
-  return Math.random() * (max - min) + min;
+    return Math.random() * (max - min) + min;
 }
 
 
 function rand_bg(index) {
-  bg = ['nhqip', '3fp01', '3fp00', 'yhojm', 'yhojp', 'yhojt', 
-        'yhojw', 'yhojx', 'yhojy', 'yhoj3', 'yhoj4', 'yhoj6', 
+    bg = ['nhqip', '3fp01', '3fp00', 'yhojm', 'yhojp', 'yhojt',
+        'yhojw', 'yhojx', 'yhojy', 'yhoj3', 'yhoj4', 'yhoj6',
         'yhoj7', 'yhoj9']
-  // bg = halloweeen //if date ==
-  // bg = holidays   // if date == 
-  // bg = valentines // if date ==
-  return bg[index]
+    // bg = halloweeen //if date ==
+    // bg = holidays   // if date ==
+    // bg = valentines // if date ==
+    return bg[index]
 }
 
 // function get_date() {
@@ -31,15 +31,6 @@ function rand_bg(index) {
 
 // Can customize color pattern:
 // https://www.npmjs.com/package/trianglify
-var pattern = Trianglify({
-  height: window.innerHeight,
-  width: window.innerWidth,
-  seed: rand_bg(rand_int(0,14)),
-  x_colors: 'random',
-  y_colors: 'match_x',
-  variance: rand_float(.7, 1),
-  stroke_width: rand_float(1, 5),
-  cell_size: rand_int(40, 200)});
 
 // var today = new Date();
 
@@ -58,7 +49,27 @@ var pattern = Trianglify({
 //   cell_size: rand_int(40, 200)});
 // }
 
+
+
+var makeBackground = function () {
+    return Trianglify({
+        height: window.innerHeight,
+        width: window.innerWidth,
+        seed: rand_bg(rand_int(0, 14)),
+        x_colors: 'random',
+        y_colors: 'match_x',
+        variance: rand_float(.7, 1),
+        stroke_width: rand_float(1, 5),
+        cell_size: rand_int(40, 200)
+    });
+};
+
 document.body.querySelector("#home").style.backgroundSize = "cover";
-document.body.querySelector("#home").style.backgroundImage =  "url(" + pattern.png() + ")";
+document.body.querySelector("#home").style.backgroundImage = "url(" + makeBackground().png() + ")";
 document.body.querySelector("#home").style.backgroundAttachment = "fixed";
 // document.body.querySelector("#home").style.opacity = 0.9;
+
+// document.body.querySelectorAll(".parallax-section").forEach(function (t) {
+//     console.log("Creating image");
+//     return t.style.backgroundImage = `url(${makeBackground.png()})`;
+// });

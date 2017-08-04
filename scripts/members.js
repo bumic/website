21ab080405b98fe7c760f96844dc90a6f1dbfbc2
Zui <zuikng@gmail.com>
2017-08-04 17:31:50 -0400

    Use the other members.json
    * Fix the JSON parsing bug.

--- scripts/members.js
+++ scripts/members.js
@@ -8,20 +8,16 @@ const vm = new Vue({
     el: '#members',
     data: {ms: members},
     mounted: function () {
-        let m = fetch("/data/members.json")
-            .then(
-                resp => resp.json()
-            ).then(
-                json => {
-                    this.ms = json.members.map(function (t) {
-                        return ({
-                            "name": t.first_name + " " + t.last_name,
-                            "pic": `image/members/${t.pic}`
-                        });
-                    });
-                })
-        ;
+        let m = fetch("members/members.json").then(resp => {
+            return resp.json()
+        }).then(json => {
+            // Todo: Check for file not file
+            // Todo: Add adviser's pictures.
+            this.ms = json.members.map(t => ({
+                "name": t.first_name + " " + t.last_name,
+                "pic": `image/members/${t.pic}`
+            }));
+            return this.ms
+        })
     }
 });
-
-

c1d4f5541353f44ac79f2429aaf4c24a7ea0999d
Zui <zuikng@gmail.com>
2017-09-11 16:07:03 -0400

    Replace parallax sections with trianglified background

--- dist/scripts/bg.js
+++ dist/scripts/bg.js
@@ -84,7 +84,7 @@ var makeBackgroundFor = function makeBackgroundFor(elements) {
     });
 };
 
-makeBackgroundFor(["#home", "#team-pic"]);
+makeBackgroundFor(["#home", "#about-pic", "#news-pic", "#members-pic", "#team-pic"]);
 
 // One for the nav bar fading in
 
